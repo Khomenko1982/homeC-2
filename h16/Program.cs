e26473c0aa0e7d7de59b086914714f042428cd01
«Andrey» <khomehko@mail.ru>
2022-08-10 15:30:16 +0300

    h16

---
+++ h16/Program.cs
@@ -0,0 +1,24 @@
+﻿// Задача 16: Напишите программу, которая принимает на вход два числа и проверяет, является ли одно число квадратом другого.
+// 5, 25 -> да
+// -4, 16 -> да
+// 25, 5 -> да
+// 8,9 -> нет
+
+Console.WriteLine("Проверяем, является ли одно число квадратом другого.");
+Console.Write("Введите первое число: ");
+int a = Convert.ToInt16(Console.ReadLine());
+Console.Write("Введите Второе число: ");
+int b = Convert.ToInt16(Console.ReadLine());
+//new Random().Next(1,10) ;
+
+if ((a*a)==b|(b*b)==a)
+{
+//int ostatok=(a%b);
+Console.Write("да ");
+//Console.Write(ostatok);
+
+}
+else
+{
+  Console.WriteLine("нет");  
+}
